85d748b7064c1302c87ca61271676e908e4cc6c0
CupOfJoe-L <jjoeliang@gmail.com>
2021-07-15 13:30:20 +0200

    Update message

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! I know it works if I write this."
+    return "Hello, World! I know it works if I write this. I also know that it is the Flask version."
