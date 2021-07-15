fefee549dd5f4e63362e7cf8a81eca113d132bcc
CupOfJoe-L <jjoeliang@gmail.com>
2021-07-15 13:42:47 +0200

    Deployslot test

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! I know it works if I write this. I also know that it is the Flask version."
+    return "Hello, World! I know it works if I write this. I also know that it is the Flask version. \n This is for Deploy."
